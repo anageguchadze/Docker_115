564bb06e4c766804610c1a44374dff21c2992f2e
Ana Geguchadze <anageguchadze22@gmail.com>
2025-03-04 14:02:18 +0100

    Add view, urls and postgres db

--- docker_pro/settings.py
+++ docker_pro/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'docker_pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresadmin22',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
